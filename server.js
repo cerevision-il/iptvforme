3617085e6bd16672dfe3dc7284b94d78ace9ada7
cerevision-il <upimobot@gmail.com>
2026-03-28 17:53:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+const express = require('express'); // 1. ייבוא הספרייה
+const app = express();              // 2. יצירת האפליקציה (לפעמים קוראים לזה server)
+
+const PORT = process.env.PORT || 3000; // 3. הגדרת הפורט
+
+app.use(express.static('.'));
+// 4. בסוף הקובץ - הפקודה להפעלת השרת:
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
